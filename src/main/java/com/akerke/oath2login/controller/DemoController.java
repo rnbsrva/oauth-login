84daac6de0bf4634e16253464f24292f99188d19
orynbasarova_a <orynbasarova_a@ezdrav.kz>
2024-05-25 18:38:33 +0600

    init

---
+++ src/main/java/com/akerke/oath2login/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.akerke.oath2login.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Secured!";
+    }
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "Goodbye, World!";
+    }
+
+}
